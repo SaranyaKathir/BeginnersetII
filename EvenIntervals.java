f9b00a4cc18d5618c7749c4f99428b336d0bd8ea
SaranyaKathir <saranyakathir128@gmail.com>
2016-10-20 15:24:04 +0530

    Create EvenIntervals.java

---
+++ EvenIntervals.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class EvenIntervals {
+public static void main(String args[]) {
+int n,i;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the numbers:");
+num=sc.nextInt();
+for(i=1;i<=n;i++) {
+if(i%2==0)
+{
+System.out.println("even numbers"+i);
+}
+}
+}
+}
