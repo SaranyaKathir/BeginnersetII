3733bcd9000217fbce21d23047592463944eb3f3
SaranyaKathir <saranyakathir128@gmail.com>
2016-10-20 14:41:56 +0530

    Create OddIntervals.java

---
+++ OddIntervals.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+  public class OddIntervals {
+    public static void main(String args[]) {
+    Scanner sc=new Scanner(System.in);
+      System.out.println("Enter two numbers for ranges:");
+    int a=sc.nextInt();
+    int b=sc.nectInt();
+  for(int i=a;i<=b;i=i+2) {
+     if(i%2==0) {
+      System.out.println(i+1);
+        } else {
+      System.out.println(i);
+      }
+    }
+  }
+}
